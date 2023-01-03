7e73f1691e33b68dda66e2bd0ed6914517443ba9
listviann <mixail.larionov.2002@mail.ru>
2023-01-04 01:00:12 +0500

    Added general view model for Authors/Index.cshtml

---
+++ TheBookstown/Models/AuthorsIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using TheBookstown.Domain.Entities;
+
+namespace TheBookstown.Models
+{
+    public class AuthorsIndexViewModel
+    {
+        public IEnumerable<Author> Authors { get; }
+        public PageViewModel PageViewModel { get; }
+        public AuthorsSortViewModel SortViewModel { get; }
+        public AuthorsFilterViewModel FilterViewModel { get; }
+
+        public AuthorsIndexViewModel(IEnumerable<Author> authors, PageViewModel pageViewModel, AuthorsSortViewModel sortViewModel, AuthorsFilterViewModel filterViewModel)
+        {
+            Authors = authors;
+            PageViewModel = pageViewModel;
+            SortViewModel = sortViewModel;
+            FilterViewModel = filterViewModel;
+        }
+    }
+}
