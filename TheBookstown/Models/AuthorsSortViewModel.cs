704d0dab0fbecf094266a3e303c25af7892b6b47
listviann <mixail.larionov.2002@mail.ru>
2023-01-04 00:50:18 +0500

    Added view model for authors sorting

---
+++ TheBookstown/Models/AuthorsSortViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TheBookstown.Models
+{
+    public enum AuthorsSortState
+    {
+        AuthorNameAsc,
+        AuthorNameDesc
+    }
+
+    public class AuthorsSortViewModel
+    {
+        public AuthorsSortState NameSort { get; }
+        public AuthorsSortState Current { get; }
+
+        public AuthorsSortViewModel(AuthorsSortState sortOrder)
+        {
+            NameSort = sortOrder == AuthorsSortState.AuthorNameAsc ? AuthorsSortState.AuthorNameDesc : AuthorsSortState.AuthorNameAsc;
+            Current = sortOrder;
+        }
+    }
+}
