5f8a74270013310e335b4ece2394b2c2420c8427
listviann <mixail.larionov.2002@mail.ru>
2023-01-04 01:13:39 +0500

    Included sorting, pagination and filtering in AuthorsController

--- TheBookstown/Controllers/AuthorsController.cs
+++ TheBookstown/Controllers/AuthorsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TheBookstown.Domain;
 using TheBookstown.Domain.Entities;
+using TheBookstown.Models;
 
 namespace TheBookstown.Controllers
 {
@@ -13,10 +14,31 @@ namespace TheBookstown.Controllers
             _dataManager = dataManager;
         }
 
-        public IActionResult Index(Guid id)
+        public IActionResult Index(Guid id, string name, int page = 1, AuthorsSortState sortOrder = AuthorsSortState.AuthorNameAsc)
         {
             if (id != default) return View("Info", _dataManager.Authors.GetAuthorById(id));
-            return View(_dataManager.Authors.GetAuthors());
+
+            int pageSize = 3;
+            IQueryable<Author> authors = _dataManager.Authors.GetAuthors();
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                authors = authors.Where(a => a.Name!.Contains(name));
+            }
+
+            authors = sortOrder switch
+            {
+                AuthorsSortState.AuthorNameDesc => authors.OrderByDescending(a => a.Name),
+                _ => authors.OrderBy(a => a.Name)
+            };
+
+            var count = authors.Count();
+            var items = authors.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            var viewModel = new AuthorsIndexViewModel(items, new PageViewModel(count, page, pageSize),
+                new AuthorsSortViewModel(sortOrder), new AuthorsFilterViewModel(name));
+
+            return View(viewModel);
         }
     }
 }
