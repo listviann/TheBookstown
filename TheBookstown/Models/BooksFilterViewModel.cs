91106a352e20446343e6807ffd4f1adbb4db4750
listviann <mixail.larionov.2002@mail.ru>
2023-01-07 21:54:08 +0500

    Add filter view model for books

---
+++ TheBookstown/Models/BooksFilterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using TheBookstown.Domain.Entities;
+
+namespace TheBookstown.Models
+{
+    public class BooksFilterViewModel
+    {
+        public string SelectedBookName { get; }
+        public Guid SelectedGenre { get; }
+        public SelectList Genres { get; }
+        public string SelectedAuthorName { get; }
+
+        public BooksFilterViewModel(List<Genre> genres, Guid genre, string bookName, string authorName)
+        {
+            genres.Insert(default, new Genre() { Name = "All" });
+            Genres = new SelectList(genres, "Id", "Name", genre);
+            SelectedAuthorName = authorName;
+            SelectedBookName = bookName;
+        }
+    }
+}
