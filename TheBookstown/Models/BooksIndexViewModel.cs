09a10b1faa3b4ad56f67bf2c44a061ed1c033893
listviann <mixail.larionov.2002@mail.ru>
2023-01-07 22:22:52 +0500

    Add general view model for books data

---
+++ TheBookstown/Models/BooksIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TheBookstown.Models
+{
+    public class BooksIndexViewModel
+    {
+        public IEnumerable<BookViewModel> Books { get; }
+        public PageViewModel PageViewModel { get; }
+        public BooksSortViewModel SortViewModel { get; }
+        public BooksFilterViewModel FilterViewModel { get; }
+
+        public BooksIndexViewModel(IEnumerable<BookViewModel> books, PageViewModel pageViewModel, BooksSortViewModel sortViewModel, BooksFilterViewModel filterViewModel)
+        {
+            PageViewModel = pageViewModel;
+            SortViewModel = sortViewModel;
+            FilterViewModel = filterViewModel;
+            Books = books;
+        }
+    }
+}
