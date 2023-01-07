f47f0e6b102067db1ff64fb69ba2834d5d7480bf
listviann <mixail.larionov.2002@mail.ru>
2023-01-07 22:06:14 +0500

    Add sort view model for books

---
+++ TheBookstown/Models/BooksSortViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace TheBookstown.Models
+{
+    public enum BooksSortState
+    {
+        BookNameAsc,
+        BooksNameDesc,
+        PriceAsc,
+        PriceDesc
+    }
+
+    public class BooksSortViewModel
+    {
+        public BooksSortState NameSort { get; }
+        public BooksSortState PriceSort { get; }
+        public BooksSortState Current { get; }
+
+        public BooksSortViewModel(BooksSortState sortOrder)
+        {
+            NameSort = sortOrder == BooksSortState.BookNameAsc ? BooksSortState.BooksNameDesc : BooksSortState.BookNameAsc;
+            PriceSort = sortOrder == BooksSortState.PriceAsc ? BooksSortState.PriceDesc : BooksSortState.PriceAsc;
+            Current = sortOrder;
+        }
+    }
+}
