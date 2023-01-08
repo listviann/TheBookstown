7dfbb349b8e6c6f9ee95727162ee7503df12c2c2
listviann <mixail.larionov.2002@mail.ru>
2023-01-08 20:42:40 +0500

    Add pagination, filtering and sorting to Index()

--- TheBookstown/Controllers/HomeController.cs
+++ TheBookstown/Controllers/HomeController.cs
@@ -15,11 +15,46 @@ namespace TheBookstown.Controllers
             _dataManager = dataManager;
         }
 
-        public IActionResult Index(Guid id)
+        public IActionResult Index(Guid id, string bookName, string authorName, Guid genre, int page = 1, BooksSortState sortOrder = BooksSortState.BookNameAsc)
         {
             if (id != default) return View("Info", _dataManager.Books.GetBookById(id));
+
+            int pageSize = 3;
+            IEnumerable<BookViewModel> books = _dataManager.Books.GetBooksModels();
+
+            if (!string.IsNullOrWhiteSpace(bookName))
+            {
+                books = books.Where(b => b.BookName!.Contains(bookName));
+            }
+            if (!string.IsNullOrWhiteSpace(authorName))
+            {
+                books = books.Where(b => b.BookAuthorName!.Contains(authorName));
+            }
+            if (genre != default)
+            {
+                books = books.Where(b => b.BookGenreId == genre);
+            }
+
+            books = sortOrder switch
+            {
+                BooksSortState.BooksNameDesc => books.OrderByDescending(b => b.BookName),
+                BooksSortState.PriceAsc => books.OrderBy(b => b.BookPrice),
+                BooksSortState.PriceDesc => books.OrderByDescending(b => b.BookPrice),
+                _ => books.OrderBy(b => b.BookName)
+            };
+
+            var count = books.Count();
+            var items = books.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            BooksIndexViewModel viewModel = new(items, 
+                new PageViewModel(count, page, pageSize),
+                new BooksSortViewModel(sortOrder), 
+                new BooksFilterViewModel(_dataManager.Genres.GetGenres().ToList(), genre, 
+                    bookName, authorName));
+
+            
             ViewBag.PageTextField = _dataManager.PagesTextFields.GetPageTextFieldByCodeWord("Home");
-            return View(_dataManager.Books.GetBooks());
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
