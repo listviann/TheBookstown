4c93b26c10f452c94340a7d146c1c47d79a6f95c
listviann <mixail.larionov.2002@mail.ru>
2023-01-09 23:15:52 +0500

    Add view model for filtering pages text fields

---
+++ TheBookstown/Areas/Admin/Models/PTFFilterViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TheBookstown.Areas.Admin.Models
+{
+    public class PTFFilterViewModel
+    {
+        public string SelectedTitle { get; }
+        public string SelectedCodeWord { get; }
+
+        public PTFFilterViewModel(string name, string codeWord)
+        {
+            SelectedTitle = name;
+            SelectedCodeWord = codeWord;
+        }
+    }
+}
