efdb06a302873a20936cc482f5013ebd0dda2d17
listviann <mixail.larionov.2002@mail.ru>
2023-01-09 23:04:58 +0500

    Add filtering view model for genres

---
+++ TheBookstown/Areas/Admin/Models/GenresFilterViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TheBookstown.Areas.Admin.Models
+{
+    public class GenresFilterViewModel
+    {
+        public string SelectedName { get; }
+
+        public GenresFilterViewModel(string name)
+        {
+            SelectedName = name;
+        }
+    }
+}
