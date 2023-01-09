3d6625c06a4b690262ca199057c9783094601b3a
listviann <mixail.larionov.2002@mail.ru>
2023-01-09 23:54:29 +0500

    Add view model for sorting users

---
+++ TheBookstown/Areas/Admin/Models/UsersSortViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TheBookstown.Areas.Admin.Models
+{
+    public enum UsersSortState
+    {
+        UserNameAsc,
+        UserNameDesc,
+    }
+    
+    public class UsersSortViewModel
+    {
+        public UsersSortState UserNameSort { get; }
+        public UsersSortState Current { get; }
+
+        public UsersSortViewModel(UsersSortState sortOrder)
+        {
+            UserNameSort = sortOrder == UsersSortState.UserNameAsc ? UsersSortState.UserNameDesc : UsersSortState.UserNameAsc;
+            Current = sortOrder;
+        }
+    }
+}
