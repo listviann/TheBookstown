a8ffb104202e82956c25e2af047dd5cf0f811d82
listviann <mixail.larionov.2002@mail.ru>
2023-01-09 23:09:12 +0500

    Add view model to sort genres

---
+++ TheBookstown/Areas/Admin/Models/GenresSortViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TheBookstown.Areas.Admin.Models
+{
+    public enum GenresSortState
+    {
+        GenreNameAsc,
+        GenreNameDesc
+    }
+
+    public class GenresSortViewModel
+    {
+        public GenresSortState NameSort { get; }
+        public GenresSortState Current { get; }
+
+        public GenresSortViewModel(GenresSortState sortOrder)
+        {
+            NameSort = sortOrder == GenresSortState.GenreNameAsc ? GenresSortState.GenreNameDesc : GenresSortState.GenreNameAsc;
+            Current = sortOrder;
+        }
+    }
+}
