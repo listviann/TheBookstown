411365d7cfd264a3c30421ec7162e5516d75a44e
listviann <mixail.larionov.2002@mail.ru>
2023-01-10 00:24:29 +0500

    Edit Index()
    - add pagination
    - add filtering
    - add sorting

--- TheBookstown/Areas/Admin/Controllers/GenresController.cs
+++ TheBookstown/Areas/Admin/Controllers/GenresController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using TheBookstown.Areas.Admin.Models;
 using TheBookstown.Domain;
 using TheBookstown.Domain.Entities;
+using TheBookstown.Models;
 using TheBookstown.Service;
 
 namespace TheBookstown.Areas.Admin.Controllers
@@ -40,9 +44,31 @@ namespace TheBookstown.Areas.Admin.Controllers
             return RedirectToAction(nameof(GenresController.Index), nameof(GenresController).CutController());
         }
 
-        public IActionResult Index()
+        public IActionResult Index(Guid id, string genreName, int page = 1, GenresSortState sortOrder = GenresSortState.GenreNameAsc)
         {
-            return View(_dataManager.Genres.GetGenres());
+            int pageSize = 3;
+            IQueryable<Genre> genres = _dataManager.Genres.GetGenres();
+
+            if (!string.IsNullOrWhiteSpace(genreName))
+            {
+                genres = genres.Where(g => g.Name!.Contains(genreName));
+            }
+
+            genres = sortOrder switch
+            {
+                GenresSortState.GenreNameDesc => genres.OrderByDescending(g => g.Name),
+                _ => genres.OrderBy(g => g.Name)
+            };
+
+            var count = genres.Count();
+            var items = genres.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            var viewModel = new GenresIndexViewModel(items, 
+                new PageViewModel(count, page, pageSize),
+                new GenresSortViewModel(sortOrder),
+                new GenresFilterViewModel(genreName));
+
+            return View(viewModel);
         }
     }
 }
