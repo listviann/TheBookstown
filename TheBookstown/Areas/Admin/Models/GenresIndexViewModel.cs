e4f16dfc7e098b6cf1d8ab8014aaeba7aab17443
listviann <mixail.larionov.2002@mail.ru>
2023-01-09 23:13:38 +0500

    Add general view model for genres index view

---
+++ TheBookstown/Areas/Admin/Models/GenresIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using TheBookstown.Domain.Entities;
+using TheBookstown.Models;
+
+namespace TheBookstown.Areas.Admin.Models
+{
+    public class GenresIndexViewModel
+    {
+        public IEnumerable<Genre> Genres { get; }
+        public PageViewModel PageViewModel { get; }
+        public GenresSortViewModel SortViewModel { get; }
+        public GenresFilterViewModel FilterViewModel { get; }
+
+        public GenresIndexViewModel(IEnumerable<Genre> genres, PageViewModel pageViewModel,
+            GenresSortViewModel sortViewModel, GenresFilterViewModel filterViewModel)
+        {
+            Genres = genres;
+            PageViewModel = pageViewModel;
+            SortViewModel = sortViewModel;
+            FilterViewModel = filterViewModel;
+        }
+    }
+}
