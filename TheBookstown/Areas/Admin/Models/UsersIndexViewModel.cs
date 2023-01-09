81efe9bc4e39ed5cdfb7a6585b2c6a0e8ad5257e
listviann <mixail.larionov.2002@mail.ru>
2023-01-09 23:57:42 +0500

    Add general view model for users index view

---
+++ TheBookstown/Areas/Admin/Models/UsersIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TheBookstown.Models;
+
+namespace TheBookstown.Areas.Admin.Models
+{
+    public class UsersIndexViewModel
+    {
+        public IEnumerable<IdentityUser> Users { get; }
+        public PageViewModel PageViewModel { get; }
+        public UsersSortViewModel SortViewModel { get; }
+        public UsersFilterViewModel FilterViewModel { get; }
+        
+        public UsersIndexViewModel(IEnumerable<IdentityUser> users, PageViewModel pageViewModel,
+            UsersSortViewModel sortViewModel, UsersFilterViewModel filterViewModel)
+        {
+            Users = users;
+            PageViewModel = pageViewModel;
+            SortViewModel = sortViewModel;
+            FilterViewModel = filterViewModel;
+        }
+    }
+}
