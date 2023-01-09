05de207cc900ba1184bb58d40b03f791f65baa6a
listviann <mixail.larionov.2002@mail.ru>
2023-01-09 23:37:28 +0500

    Add general view model for pages text fields index view

---
+++ TheBookstown/Areas/Admin/Models/PTFIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using TheBookstown.Domain.Entities;
+using TheBookstown.Models;
+
+namespace TheBookstown.Areas.Admin.Models
+{
+    public class PTFIndexViewModel
+    {
+        public IEnumerable<PageTextField> PagesTextFields { get; }
+        public PageViewModel PageViewModel { get; }
+        public PTFSortViewModel SortViewModel { get; }
+        public PTFFilterViewModel FilterViewModel { get; }
+
+        public PTFIndexViewModel(IEnumerable<PageTextField> pagesTextFields, PageViewModel pageViewModel,
+            PTFSortViewModel sortViewModel, PTFFilterViewModel filterViewModel)
+        {
+            PagesTextFields = pagesTextFields;
+            PageViewModel = pageViewModel;
+            SortViewModel = sortViewModel;
+            FilterViewModel = filterViewModel;
+        }
+    }
+}
