1d1b29b2c3364e4bdd4bcce10074d894d8e10c0f
listviann <mixail.larionov.2002@mail.ru>
2023-01-09 23:29:27 +0500

    Add view model for sorting pages text fields

---
+++ TheBookstown/Areas/Admin/Models/PTFSortViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace TheBookstown.Areas.Admin.Models
+{
+    public enum PTFSortState
+    {
+        TitleAsc,
+        TitleDesc,
+        CodeWordAsc,
+        CodeWordDesc,
+    }
+
+    public class PTFSortViewModel
+    {
+        public PTFSortState TitleSort { get; }
+        public PTFSortState CodeWordSort { get; }
+        public PTFSortState Current { get; }
+
+        public PTFSortViewModel(PTFSortState sortOrder)
+        {
+            TitleSort = sortOrder == PTFSortState.TitleAsc ? PTFSortState.TitleDesc : PTFSortState.TitleAsc;
+            CodeWordSort = sortOrder == PTFSortState.CodeWordAsc ? PTFSortState.TitleDesc : PTFSortState.TitleAsc;
+            Current = sortOrder;
+        }
+    }
+}
