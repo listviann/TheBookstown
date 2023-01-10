4e938b5478c1efe3079c3ccf4745484ac2398c97
listviann <mixail.larionov.2002@mail.ru>
2023-01-10 21:18:58 +0500

    Add pagination, filtering and sorting to Index()

--- TheBookstown/Areas/Admin/Controllers/UsersController.cs
+++ TheBookstown/Areas/Admin/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TheBookstown.Areas.Admin.Models;
 using TheBookstown.Domain;
 using TheBookstown.Service;
+using TheBookstown.Models;
 
 namespace TheBookstown.Areas.Admin.Controllers
 {
@@ -21,10 +24,33 @@ namespace TheBookstown.Areas.Admin.Controllers
             return RedirectToAction(nameof(UsersController.Index), nameof(UsersController).CutController());
         }
 
-        public IActionResult Index(Guid id)
+        public IActionResult Index(Guid id, string userName, int page = 1, UsersSortState sortOrder = UsersSortState.UserNameAsc)
         {
             if (id != default) return View("Info", _dataManager.Users.GetUserById(id));
-            return View(_dataManager.Users.GetUsers());
+
+            int pageSize = 3;
+            IQueryable<IdentityUser> users = _dataManager.Users.GetUsers();
+
+            if (!string.IsNullOrWhiteSpace(userName))
+            {
+                users = users.Where(u => u.UserName.Contains(userName));
+            }
+
+            users = sortOrder switch
+            {
+                UsersSortState.UserNameDesc => users.OrderByDescending(u => u.UserName),
+                _ => users.OrderBy(u => u.UserName)
+            };
+
+            var count = users.Count();
+            var items = users.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            var viewModel = new UsersIndexViewModel(users,
+                new PageViewModel(count, page, pageSize),
+                new UsersSortViewModel(sortOrder),
+                new UsersFilterViewModel(userName));
+
+            return View(viewModel);
         }
     }
 }
