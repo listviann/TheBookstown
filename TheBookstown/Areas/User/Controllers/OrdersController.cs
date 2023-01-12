6b974af8b0f1e0c649da0563b56a748f567adee0
listviann <mixail.larionov.2002@mail.ru>
2023-01-12 16:34:52 +0500

    Add some mechanisms that must prevent user from deleting from empty collection

--- TheBookstown/Areas/User/Controllers/OrdersController.cs
+++ TheBookstown/Areas/User/Controllers/OrdersController.cs
@@ -17,20 +17,24 @@ namespace TheBookstown.Areas.User.Controllers
 
         public IActionResult Index()
         {
-            return View(_dataManager.OrderItems.GetOrdersHistory());
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            ViewBag.PageTextField = _dataManager.PagesTextFields.GetPageTextFieldByCodeWord("Orders");
+            return View(_dataManager.OrderItems.GetOrdersHistory().Where(o => o.UserId == new Guid(userId)));
         }
 
         [HttpPost]
         public IActionResult DoOrder()
         {
+            if (_dataManager.UserCartItems.GetUserCart() == null) return BadRequest();
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             _dataManager.OrderItems.Save(new Guid(userId));
-            return RedirectToAction(nameof(TheBookstown.Controllers.HomeController.Index), nameof(TheBookstown.Controllers.HomeController).CutController());
+            return RedirectToAction(nameof(TheBookstown.Controllers.HomeController.Index), nameof(TheBookstown.Controllers.HomeController).CutController(), new { area = "" });
         }
 
         [HttpPost]
         public IActionResult ClearHistory()
         {
+            if (_dataManager.OrderItems.GetOrdersHistory() == null) return BadRequest();
             _dataManager.OrderItems.ClearOrdersHistory();
             return RedirectToAction(nameof(OrdersController.Index), nameof(OrdersController).CutController());
         }
