3b4f43855a3501b73128f51db65f45a459983254
listviann <mixail.larionov.2002@mail.ru>
2023-01-18 19:41:59 +0500

    Add entity to store data and details about order done by user

--- TheBookstown/Domain/Entities/OrderItem.cs
+++ TheBookstown/Domain/Entities/OrderItem.cs
@@ -5,11 +5,11 @@ namespace TheBookstown.Domain.Entities
     // order based on cart items
     public class OrderItem : BaseEntity
     {
+        public List<OrderDetail> OrderDetails { get; set; } = new();
+        
         public int TotalCost { get; set; }
 
         [ForeignKey("UserId")]
         public Guid UserId { get; set; }
-
-        public List<UserCartItem> CartItems { get; set; } = new();
     }
 }
